f5a19d319129c1c32de86dbd056b2249bab7cb4d
Parveen Kumar <parveen.kumar6@gmail.com>
2022-05-08 17:18:49 +0530

    Adding comments in code for bin Search

--- proj-files/bin_search.py
+++ proj-files/bin_search.py
@@ -1,25 +1,25 @@
 def binary_search(arr, low, high, x):
  
-    
+    #if high >= low
     if high >= low:
- 
+        #get the middle item
         mid = (high + low) // 2
  
-        
+        #check if item at middle
         if arr[mid] == x:
             return mid
  
         
-        
+        #else make recursive call on left
         elif arr[mid] > x:
             return binary_search(arr, low, mid - 1, x)
  
-        
+        #othewise make recursive call on right
         else:
             return binary_search(arr, mid + 1, high, x)
  
     else:
-        
+        #else return -1 if item not found.
         return -1
  
 
